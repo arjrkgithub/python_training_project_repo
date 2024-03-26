f8cf204ad1e42cfcc2b05929e998d66f06c3ce5d
hp sys <arj.rajkumar@gmail.com>
2024-03-26 23:01:22 +0400

    initial changes updated

---
+++ mySecondPython.py
@@ -0,0 +1,10 @@
+
+from myFirstPython import Greeter
+
+informal_greeter = Greeter("Pythonista")
+informal_greeter.greet()
+
+informal_greeter = Greeter("Pythonista",True)
+informal_greeter.greet()
+
+print("process completed")
